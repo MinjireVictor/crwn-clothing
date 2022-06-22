a53b65dfffee83f0d449600f6ce056e415b153ec
Minjire Victor <markminj@gmail.com>
2022-06-22 11:35:24 +0300

    implemented cart dropdown

--- src/Routes/navigation/navigation.component.jsx
+++ src/Routes/navigation/navigation.component.jsx
@@ -4,10 +4,14 @@ import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
 import './navigation.styles.scss'
 import { UserContext } from '../../context/user.context'
 import  {signOutUser} from '../../utils/firebase/firebase.utils'
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown'
+import { CartContext } from '../../context/cart.context'
 
 
 const Navigation = ()=>{
   const {currentUser}=useContext(UserContext)
+  const {isCartOpen}=useContext(CartContext)
   
     return (
       <Fragment>
@@ -21,7 +25,9 @@ const Navigation = ()=>{
                 <Link className='nav-link' to='/shop' >SHOP</Link>
                 {currentUser? <span className='nav-link' onClick={signOutUser}>Sign Out</span>
                 : <Link className='nav-link' to='/auth' >Sign In</Link>}
+                <CartIcon/>
             </div>
+            {isCartOpen && <CartDropdown/>}
             </div>
         <Outlet/>
       </Fragment>
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,16 +3,8 @@ import Home from "../Routes/home/home.components"
 import {Routes, Route} from 'react-router-dom'
 import Navigation from "../Routes/navigation/navigation.component"
 import Authentication from '../Routes/authentication/authentication.component';
+import Shop from '../Routes/shop/shop.component';
 
-
-
-
-const Shop =()=>{
-  return <div>
-    <h1>Shop</h1>
-
-  </div>
-}
 
 
 function App() {
